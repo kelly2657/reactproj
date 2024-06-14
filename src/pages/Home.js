39bc8207c1d904369477d8b2780b7189fb615f52
mj <ui2939@gmail.com>
2024-06-14 13:59:18 +0900

    edit Home, prof

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home(){
+    return (
+        <div>
+            <p>홈입니다</p>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Profile(){
+    return (
+        <div>
+            <p>프로필입니다</p>
+        </div>
+    )
+}
+
+export default Profile;
