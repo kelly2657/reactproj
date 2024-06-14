d07946c180c0da0362213b143131dfd216b5ce55
Hyemin Oh <orionrora@naver.com>
2024-06-14 23:49:20 +0900

    App.js 라우

--- src/App.js
+++ src/App.js
@@ -1,22 +1,113 @@
-import React from 'react';
+import React, { useReducer, useRef, useEffect } from "react";
 import './App.css';
-import Header from './component/Header';
-import Profile from './component/Profile';
-import DarkMode from './component/Darkmode';
-import Card from './component/Card';
-import './styles/Darkmode.css';
-import Detail from './component/detail';
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Edit from "./pages/Edit";
 
+import './styles/Darkmode.css';
+import { Routes, Route } from 'react-router-dom';
+
+const mockData = [
+  {
+    id: "mock1",
+    date: new Date("2024-05-31").getTime(),//new Date().getTime() - 1,
+    content: "mock1이 어쩌구 저쩌구... ",
+    title: "mock1 입니다",
+  },
+  {
+    id: "mock2",
+    date: new Date("2024-06-14").getTime(),//new Date().getTime() - 2,
+    content: "mock2이 어쩌구 저쩌구...",
+    title: "mock1 입니다",
+  },
+  {
+    id: "mock3",
+    date: new Date("2024-06-13").getTime(),//new Date().getTime() - 3,
+    content: "mock3이 어쩌구 저쩌구...",
+    title: "mock1 입니다",
+    },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      return [action.data, ...state];
+    }
+    case "UPDATE": {
+      return state.map((it) =>
+        String(it.id) === String(action.data.id) ? { ...action.data } : it
+      );
+    }
+    case "DELETE": {
+      return state.filter((it) => String(it.id) !== String(action.targetId));
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export const BlogStateContext = React.createContext();
+export const BlogDispatchContext = React.createContext();
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  useEffect(() => {
+    dispatch({
+      type: "INIT",
+      data: mockData,
+    });
+  }, []);
+
+  const onCreate = (date, content, title) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current,
+        date: new Date(date).getTime(),
+        content,
+        title
+      },
+    });
+    idRef.current += 1;
+  };
+
+  const onUpdate = (targetId, date, content, title) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        title,
+      },
+    });
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
+  };
+
   return (
-    <div className="App">
-      <DarkMode></DarkMode>
-      <Header></Header>
-      <Profile></Profile>
-      <Detail></Detail>
-      <Card></Card>
-    </div>
+    <BlogStateContext.Provider value={data}>
+      <BlogDispatchContext.Provider value={{onCreate, onUpdate, onDelete}}>
+          <div className='App'>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/blog/:id" element={<Blog/>}/>
+              <Route path="/edit/:id" element={<Edit/>}/>
+            </Routes>
+          </div>
+      </BlogDispatchContext.Provider>
+    </BlogStateContext.Provider>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import Header from '../component/Header';
+import Profile from '../component/Profile';
+import DarkMode from '../component/Darkmode';
+import Card from '../component/Card';
+import '../styles/Darkmode.css';
+import Detail from '../component/detail';
 function Home(){
     return (
         <div>
-            <p>홈입니다</p>
+            {/* <p>홈입니다</p> */}
+            <DarkMode></DarkMode>
+            <Header></Header>
+            <Profile></Profile>
+            <Detail></Detail>
+            <Card></Card>
         </div>
     )
 }
