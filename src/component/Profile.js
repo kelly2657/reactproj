12571765e00442a0ac0fbad9018a20ea765114c2
mj <ui2939@gmail.com>
2024-06-14 22:27:44 +0900

    detail page Category' link / img

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import sample from '../img/Profile-img-sample.png';
-import { NavLink } from "react-router-dom";
+import { NavLink, Route, Routes } from "react-router-dom";
 import '../styles/Profile.css';
 
 
@@ -12,11 +12,13 @@ function Profile() {
 
             <div id="introducing-blog"><p>제 블로그에는 ~~~ 이런 주제의 글을 올리고 있습니다</p></div>
             <div id="Category"><ul className="no-dot">
-                <li>음식</li>
-                <li>여행</li>
-                <li>축제</li>
-                <li>공부</li>
-            </ul></div>
+                <li><NavLink to="/foods">음식</NavLink></li>
+                <li><NavLink to="/trip">여행</NavLink></li>
+                <li><NavLink to="/festival">축제</NavLink></li>
+                <li><NavLink to="/study">공부</NavLink></li>
+            </ul>
+
+            </div>
         </div>
     )
 }
--- src/component/detail.js
+++ src/component/detail.js
@@ -2,50 +2,104 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import '../styles/Detail.css';
 
-function Foods() {
-    return (
-        <dev>
 
-        </dev>
+// require.context를 사용하여 폴더 내 모든 이미지 가져오기
+const importAll = (requireContext) => 
+    requireContext.keys().map(requireContext);
+
+const imagePath = importAll(require.context('../img', true, /\.(png|jpe?g|svg)$/));
+console.log(imagePath);
+
+function Foods() {
+    let img = [];
+    for ( let i=0; i<imagePath.length; i++){
+        if(imagePath[i].substr(13, 6) === "/food-")
+            img.push(<div className="img-Card">
+                <img className="img-size" key={i} src={imagePath[i]}  width="600%"></img>
+                </div>);
+    }
+
+    return (
+        <div className="img-box">
+            {img}
+        </div>
 
     )
 }
 
 function Trip() {
+    let img = [];
+    for ( let i=0; i<imagePath.length; i++){
+        if(imagePath[i].substr(13, 6) === "/trip-")
+            img.push(<div className="img-Card">
+                <img className="img-size" key={i} src={imagePath[i]}  width="600%"></img>
+                </div>);
+    }
+
     return (
-        <dev>
-            
-        </dev>
+        <div className="img-box">
+            {img}
+        </div>
     )
 }
 
 function Festival() {
+    let img = [];
+    for ( let i=0; i<imagePath.length; i++){
+        if(imagePath[i].substr(13, 10) === "/festival-")
+            img.push(<div className="img-Card">
+                <img className="img-size" key={i} src={imagePath[i]}  width="600%"></img>
+                </div>);
+    }
+
     return (
-        <dev>
-            
-        </dev>
+        <div className="img-box">
+            {img}
+        </div>
     )
 }
 
 function Study() {
+    let img = [];
+    for ( let i=0; i<imagePath.length; i++){
+        if(imagePath[i].substr(13, 7) === "/study-")
+            img.push(<div className="img-Card">
+                <img className="img-size" key={i} src={imagePath[i]}  width="600%"></img>
+                </div>);
+    }
+
     return (
-        <dev>
+        <div className="img-box">
+            {img}
+        </div>
+    )
+}
+
+function Content() {
+    const ContentsTitle = <h2>제주도 여행 2박3일 다녀왔습니다</h2>;
+    const ContentsBody = <p> 제주도 … 2박 … 3일 … </p>;
+    return (
+        <div id="Contents-box">
+        <article className="Content">
+            {ContentsTitle}
+            {ContentsBody}
             
-        </dev>
+        </article>
+        </div>
     )
 }
 
 
-const ContentsTitle = <h2>제주도 여행 2박3일 다녀왔습니다</h2>;
-const ContentsBody = <p> 제주도 … 2박 … 3일 … </p>;
-function Contents(){
-    return(
-        <div id="Contents-box">
-            <div className="Content">
-                <article>
-                {ContentsTitle}
-                {ContentsBody}
-                </article>
+function Contents() {
+    return (
+        <div >
+            <div >
+            <Routes>
+                <Route path="/foods" element={<Foods />}></Route>
+                <Route path="/trip" element={<Trip />}></Route>
+                <Route path="/festival" element={<Festival />}></Route>
+                <Route path="/study" element={<Study />}></Route>
+            </Routes>
             </div>
         </div>
     )
@@ -55,7 +109,8 @@ function Contents(){
 function Detail() {
     return (
         <div className="Viewer">
-        <Contents></Contents>
+            <Contents></Contents>
+
         </div>
     )
 }
