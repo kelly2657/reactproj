a5943b8b4d528735fa2ba0c34a42da7a320298c2
mj <ui2939@gmail.com>
2024-06-14 13:56:28 +0900

    add Home / Prof component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Header from './pages/Home'; 
+import Profile from './pages/Profile'; 
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Profile></Profile>
     </div>
   );
 }
 
 export default App;
+
